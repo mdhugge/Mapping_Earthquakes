78540734dbd3fae6fce43fabae400ea47666f5d3
Maneet Dhugge <maneetdhugge@Maneets-MacBook-Pro.local>
2021-03-23 17:38:51 -0400

    Multiple Points

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -11,6 +11,7 @@ let marker = L.circle([34.0522, -118.2437], {
     fillColor:'#ffffa1'
  }).addTo(map);
 
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
